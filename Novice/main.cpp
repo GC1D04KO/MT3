45e12107b8b31b3f856b7032c482e026565404bb
GC1D04KO <k023g1067@g.neec.ac.jp>
2024-07-30 14:41:31 +0900

    00-05の確認課題

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,12 +1,84 @@
 #include <Novice.h>
+#include <cmath>
 
 const char kWindowTitle[] = "GC2C_03_コウ_カイシン";
+
+static const int kColumnWidth = 60;
+static const int kRowHeight = 20;
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+struct Matrix4x4 {
+	float m[4][4];
+};
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 affine;
+
+	affine.m[0][0] = scale.x * (((1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0) * std::cos(rotate.z)) + ((1 * 0 + 0 * 1 + 0 * 0 + 0 * 0) * std::sin(rotate.z)) +
+	                            ((1 * std::sin(-rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0) * 0) + ((1 * 0 + 0 * 0 + 0 * 0 + 0 * 1) * 0));
+	affine.m[0][1] = scale.x * (((1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0) * std::sin(rotate.z)) + ((1 * 0 + 0 * 1 + 0 * 0 + 0 * 0) * std::cos(rotate.z)) +
+	                            ((1 * std::sin(-rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0) * 0) + ((1 * 0 + 0 * 0 + 0 * 0 + 0 * 1) * 0));
+	affine.m[0][2] = scale.x * (((1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0) * 0) + ((1 * 0 + 0 * 1 + 0 * 0 + 0 * 0) * 0) +
+	                            ((1 * std::sin(-rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0) * 1) + ((1 * 0 + 0 * 0 + 0 * 0 + 0 * 1) * 0));
+	affine.m[0][3] = 0;
+
+	affine.m[1][0] = scale.y * (((0 * std::cos(rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::cos(rotate.z)) +
+	                            ((0 * 0 + std::cos(rotate.x) * 1 + std::sin(rotate.x) * 0 + 0 * 0) * std::sin(-rotate.z) +
+	                             ((0 * std::sin(-rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) +
+	                             ((0 * 0 + std::cos(rotate.x) * 0 + std::sin(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[1][1] = scale.y * (((0 * std::cos(rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::sin(rotate.z)) +
+	                            ((0 * 0 + std::cos(rotate.x) * 1 + std::sin(rotate.x) * 0 + 0 * 0) * std::cos(rotate.z) +
+	                             ((0 * std::sin(-rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) +
+	                             ((0 * 0 + std::cos(rotate.x) * 0 + std::sin(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[1][2] =
+	    scale.y *
+	    (((0 * std::cos(rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::sin(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::cos(rotate.x) * 1 + std::sin(rotate.x) * 0 + 0 * 0) * 0) +
+	     ((0 * std::sin(-rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0) * 1) + ((0 * 0 + std::cos(rotate.x) * 0 + std::sin(rotate.x) * 0 + 0 * 1) * 0));
+	affine.m[1][3] = 0;
+
+	affine.m[2][0] = scale.z * (((0 * std::cos(rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::cos(rotate.z) +
+	                             ((0 * 0 + std::sin(-rotate.x) * 1 + std::cos(rotate.x) * 0 + 0 * 0) * std::sin(-rotate.z)) +
+	                             ((0 * std::sin(-rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) +
+	                             ((0 * 0 + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[2][1] = scale.z * (((0 * std::cos(rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::sin(rotate.z) +
+	                             ((0 * 0 + std::sin(-rotate.x) * 1 + std::cos(rotate.x) * 0 + 0 * 0) * std::cos(-rotate.z)) +
+	                             ((0 * std::sin(-rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) +
+	                             ((0 * 0 + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[2][2] =
+	    scale.z *
+	    (((0 * std::cos(rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::sin(-rotate.x) * 1 + std::cos(rotate.x) * 0 + 0 * 0) * 0) +
+	     ((0 * std::sin(-rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0) * 1) + ((0 * 0 + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * 0 + 0 * 1) * 0));
+	affine.m[2][3] = 0;
+
+	affine.m[3][0] = translate.x;
+	affine.m[3][1] = translate.y;
+	affine.m[3][2] = translate.z;
+	affine.m[3][3] = 1;
+	return affine;
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + 20, "%6.02f", matrix.m[row][column]);
+		}
+	}
+	Novice::ScreenPrintf(x, y, "%s", label);
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 scale{1.2f, 0.79f, -2.1f};
+	Vector3 rotate{0.4f, 1.43f, -0.8f};
+	Vector3 translate{2.7f, -4.15f, 1.57f};
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -33,6 +105,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+
 		///
 		/// ↑描画処理ここまで
 		///
@@ -50,3 +124,4 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Finalize();
 	return 0;
 }
+
